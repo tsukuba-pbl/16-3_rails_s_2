daa0ae6327fb8f1c3a4a22c1b412df9edf9c381b
shunsukeumemura <umes0714@gmail.com>
2017-01-12 17:30:16 +0900

    schemaを修正

--- db/schema.rb
+++ db/schema.rb
@@ -21,14 +21,6 @@ ActiveRecord::Schema.define(version: 20170112075318) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "buildings", force: :cascade do |t|
-    t.string   "name"
-    t.string   "adress"
-    t.string   "remarks"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "ft_items", force: :cascade do |t|
     t.integer  "item_id"
     t.float    "top"
@@ -108,7 +100,6 @@ ActiveRecord::Schema.define(version: 20170112075318) do
     t.string   "madori_cache"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.integer  "building_id"
     t.integer  "article_id"
   end
 
