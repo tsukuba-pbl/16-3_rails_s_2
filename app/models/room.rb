f85592d68e3a7740e21f2708ff83c0a5ad60261c
lghpws <lghpws@gmail.com>
2017-01-29 14:17:52 +0000

    roomに一意性ぽいバリデーション追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -10,9 +10,14 @@ class Room < ActiveRecord::Base
   validates :height, presence: true
   
   validate :exsitence_of_article_id
-
+  validate :uniqueness_of_article_id
+  
   private
     def exsitence_of_article_id
       errors.add(:article_id, "登録する部屋がどの物件の部屋であるか不明です　指定されたidの物件がみつかりません") unless Article.pluck(:id).include?(article_id)
+    end
+    
+    def uniqueness_of_article_id
+      errors.add(:article_id, "すみません　一つの物件に登録できる部屋は一つまでです すでにその物件には部屋が登録されています")  if Room.find_by(article_id: article_id)
     end
 end
