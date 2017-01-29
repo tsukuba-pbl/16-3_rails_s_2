b7955e39cb8e55497a23e9c5829e5fd235ad0b9b
lghpws <lghpws@gmail.com>
2017-01-29 01:28:52 +0000

    roomのバリデーション追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,5 +2,10 @@ class Room < ActiveRecord::Base
   mount_uploader :madori, MadoriUploader
   has_many :uxsers, dependent: :destroy
   has_many :ft_layouts
+  
   belongs_to :article
+  
+  validates :article_id, presence: true
+  validates :width, presence: true
+  validates :height, presence: true
 end
