d4f161d774d84bef18f292ad44bbf15b91979b20
lghpws <lghpws@gmail.com>
2017-01-29 05:59:11 +0000

    画像表のバリデーション追加

--- app/models/uxser.rb
+++ app/models/uxser.rb
@@ -1,4 +1,19 @@
 class Uxser < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
   belongs_to :room
+  
+  validates :zahyo_x, presence: true
+  validates :zahyo_y, presence: true
+  validates :zahyo_z, presence: true
+  validates :room_id, presence: true
+  validates :room_depth, presence: true
+  validates :room_width, presence: true
+  validates :offset, presence: true
+
+  validate :exsitence_of_room_id
+
+  private
+    def exsitence_of_room_id
+      errors.add(:room_id, "指定されたidの物件がない") unless Room.pluck(:id).include?(room_id)
+    end
 end
